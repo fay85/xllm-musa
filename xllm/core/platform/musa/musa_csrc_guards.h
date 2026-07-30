7b0b128ab7acc1e2e87cf2844eca142e14793364
mccxadmin <fay85@me.com>
2026-07-30 14:08:03 +0800

    refactor: remove useless USE_MUSA in musa_csrc_guards.h.

--- xllm/core/platform/musa/musa_csrc_guards.h
+++ xllm/core/platform/musa/musa_csrc_guards.h
@@ -15,11 +15,9 @@ limitations under the License.
 
 #pragma once
 
-#if defined(USE_MUSA)
 #define EIGEN_NO_CUDA
 #define EIGEN_NO_GPU
 // Include only from MUSA graph TUs that also pull ATen/cuda/* compatibility
 // headers.
 #define TORCH_MUSA_CSRC_CORE_MUSACACHINGALLOCATOR_H_
 #define TORCH_MUSA_CSRC_CORE_MUSAGRAPHSC10UTILS_H_
-#endif
